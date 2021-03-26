d839fb12e5b533f8a3c33eaa61c5fcfce1c36f02
Bence Imre Kiss <kissbenceimre99@gmail.com>
2021-03-26 21:56:55 +0100

    drag

---
+++ Assets/Scripts/UI/Dragable.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Dragable : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler
+{
+    public Canvas canvas;
+    RectTransform rectTransform;
+    CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 0.5f;
+        canvasGroup.blocksRaycasts = false;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+}
