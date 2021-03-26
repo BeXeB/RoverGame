a7a71fd49653b56505727c18c9b8aab38c7dffd2
Bence Imre Kiss <kissbenceimre99@gmail.com>
2021-03-26 23:56:17 +0100

    commit so i can rebase

--- Assets/Scripts/UI/Slot.cs
+++ Assets/Scripts/UI/Slot.cs
@@ -3,11 +3,13 @@ using UnityEngine.EventSystems;
 
 public class Slot : MonoBehaviour, IDropHandler
 {
+    [SerializeField] int slotIndex;
     public void OnDrop(PointerEventData eventData)
     {
         if (eventData.pointerDrag != null)
         {
-            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            eventData.pointerDrag.GetComponent<RectTransform>().position = GetComponent<RectTransform>().position;
+            
         }
     }
 }
