1af8276a0d8ba34fa80505cca64521cb2fd521a7
Bence Imre Kiss <kissbenceimre99@gmail.com>
2021-03-27 16:04:28 +0100

    ui

--- Assets/Scripts/UI/ActionSlotController.cs
+++ Assets/Scripts/UI/ActionSlotController.cs
@@ -8,11 +8,6 @@ public class ActionSlotController : MonoBehaviour
     [SerializeField] Transform parent;
     private List<ActionSlot> slots;
 
-    private void Start()
-    {
-        Instantiate(prefab, parent.position, parent.rotation, parent);
-    }
-
     public void AddSlot(ActionSlot slot)
     {
         slots.Add(slot);
