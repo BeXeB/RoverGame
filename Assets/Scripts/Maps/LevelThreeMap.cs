85c971a891fe5981afe4c70c1b07146ef943e12b
Balazs <1074285@ucn.dk>
2021-03-27 19:59:12 +0100

    mapz

---
+++ Assets/Scripts/Maps/LevelThreeMap.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelThreeMap : Map
+{
+    public override int[,] getMap()
+    {
+        int[,] map = new int[11, 5]
+        {
+            {2, 2, 2, 2, 2},
+            {2, 0, 2, 2, 2},
+            {2, 1, 2, 2, 2},
+            {2, 1, 2, 2, 2},
+            {2, 1, 1, 1, 2},
+            {2, 2, 2, 1, 2},
+            {2, 2, 2, 1, 2},
+            {2, 2, 2, 1, 2},
+            {2, 2, 2, 1, 2},
+            {2, 2, 2, 1, 2},
+            {2, 2, 2, 2, 2},
+        };
+        return map;
+    }
+}
---
+++ Assets/Scripts/Maps/LevelTwoMap.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTwoMap : Map
+{
+    public override int[,] getMap()
+    {
+        int[,] map = new int[11,3]
+        {
+            {2, 2, 2},
+            {2, 0, 2},
+            {2, 1, 2},
+            {2, 1, 2},
+            {2, 1, 2},
+            {2, 1, 2},
+            {2, 1, 2},
+            {2, 1, 2},
+            {2, 1, 2},
+            {2, 1, 2},
+            {2, 2, 2},
+        };
+        return map;
+    }
+}
