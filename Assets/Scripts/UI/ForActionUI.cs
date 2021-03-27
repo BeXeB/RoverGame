d45ca059fc13dc33185fdd606e15e7198f962956
Bence Imre Kiss <kissbenceimre99@gmail.com>
2021-03-27 13:47:53 +0100

    main manu

--- Assets/Scripts/UI/ForActionUI.cs
+++ Assets/Scripts/UI/ForActionUI.cs
@@ -6,45 +6,6 @@ public class ForActionUI : ActionUI
 {
     [SerializeField] GameObject ActionPrefab;
     [SerializeField] TMP_InputField inputField;
-    [SerializeField] List<ForSlot> slots;
-    [SerializeField] GameObject slotPrefab;
-    [SerializeField] Transform slotsParent;
-
-    public void AddSlot()
-    {
-        GameObject go = Instantiate(slotPrefab);
-        slots.Add(go.GetComponent<ForSlot>());
-        go.GetComponent<RectTransform>().SetParent(slotsParent);
-        SlotsChanged();
-    }
-
-    public void AddSlot(int index)
-    {
-        GameObject go = Instantiate(slotPrefab);
-        slots.Insert(index, go.GetComponent<ForSlot>());
-        go.GetComponent<RectTransform>().SetParent(slotsParent);
-        SlotsChanged();
-    }
-
-    public void RemoveSlot(int index)
-    {
-        GameObject go = slots[index].gameObject;
-        slots.Remove(slots[index]);
-        Destroy(go);
-        SlotsChanged();
-    }
-
-    void SlotsChanged()
-    {
-        for (int i = 0; i < slots.Count; i++)
-        {
-            slots[i].slotIndex = i;
-            if (slots[i].GetComponentInChildren<ActionUI>())
-            {
-                slots[i].GetComponentInChildren<ActionUI>().slotIndex = i;
-            }
-        }
-    }
 
     void UpdateAction()
     {
---
+++ Assets/Scripts/UI/SensorUI.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensorUI : MonoBehaviour
+{
+    public bool inSlot;
+    public int slotIndex;
+    public Sensor sensor;
+}
