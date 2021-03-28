04cc39e5beea7bdca87badbfc0751a304c01c2de
Bence Imre Kiss <kissbenceimre99@gmail.com>
2021-03-28 11:14:01 +0200

    movement 1

--- Assets/Scripts/UI/MoveBackwardActionUI.cs
+++ Assets/Scripts/UI/MoveBackwardActionUI.cs
@@ -4,20 +4,10 @@ using TMPro;
 public class MoveBackwardActionUI : ActionUI
 {
     [SerializeField] GameObject ActionPrefab;
-    [SerializeField] TMP_InputField inputField;
-
-    void UpdateAction()
-    {
-        int number;
-        if (int.TryParse(inputField.text, out number) && inSlot)
-        {
-            ((MoveAction)action).amount = number * -1;
-        }
-    }
     private void Start()
     {
-        inputField.onEndEdit.AddListener(delegate { UpdateAction(); });
         GameObject go = Instantiate(ActionPrefab);
         action = go.GetComponent<MoveAction>();
+        ((MoveAction)action).amount = -1;
     }
 }
